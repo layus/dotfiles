96ffa3809347b082ce6322e3987252039ef4de33
Guillaume Maudoux <layus.on@gmail.com>
2021-10-22 11:00:27 +0200

    add more config

---
+++ home/old/kanshi/docked.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+swaymsg workspace 0,  move workspace to output eDP-1
+swaymsg workspace 1,  move workspace to output '"Dell Inc. DELL U2415 7MT0196H25AS"' # $(swaymsg -p -t get_outputs | grep "Dell Inc. DELL U2415 7MT0196H25AS" | cut -f2 -d" ")
+swaymsg workspace 2,  move workspace to output '"Dell Inc. DELL U2415 7MT0196H25AS"' # $(swaymsg -p -t get_outputs | grep "Dell Inc. DELL U2415 7MT0196H25AS" | cut -f2 -d" ")
+swaymsg workspace 3,  move workspace to output '"Dell Inc. DELL U2415 7MT0196H25AS"' # $(swaymsg -p -t get_outputs | grep "Dell Inc. DELL U2415 7MT0196H25AS" | cut -f2 -d" ")
+swaymsg workspace 4,  move workspace to output '"Dell Inc. DELL U2415 7MT0196H25AS"' # $(swaymsg -p -t get_outputs | grep "Dell Inc. DELL U2415 7MT0196H25AS" | cut -f2 -d" ")
+swaymsg workspace 5,  move workspace to output '"Dell Inc. DELL U2415 7MT0196H25AS"' # $(swaymsg -p -t get_outputs | grep "Dell Inc. DELL U2415 7MT0196H25AS" | cut -f2 -d" ")
+swaymsg workspace 6,  move workspace to output '"Dell Inc. DELL U2415 7MT0196H25AS"' # $(swaymsg -p -t get_outputs | grep "Dell Inc. DELL U2415 7MT0196H25AS" | cut -f2 -d" ")
+swaymsg workspace 7,  move workspace to output '"Dell Inc. DELL U2415 7MT0196H25AS"' # $(swaymsg -p -t get_outputs | grep "Dell Inc. DELL U2415 7MT0196H25AS" | cut -f2 -d" ")
+swaymsg workspace 8,  move workspace to output '"Dell Inc. DELL U2415 7MT0196H25AS"' # $(swaymsg -p -t get_outputs | grep "Dell Inc. DELL U2415 7MT0196H25AS" | cut -f2 -d" ")
+swaymsg workspace 9,  move workspace to output '"Dell Inc. DELL U2415 7MT0196H25AS"' # $(swaymsg -p -t get_outputs | grep "Dell Inc. DELL U2415 7MT0196H25AS" | cut -f2 -d" ")
+swaymsg workspace 10, move workspace to output '"Dell Inc. DELL U2415 7MT0196H25AS"' # $(swaymsg -p -t get_outputs | grep "Dell Inc. DELL U2415 7MT0196H25AS" | cut -f2 -d" ")
+swaymsg workspace 1
