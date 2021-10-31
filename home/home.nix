bea0afc7d42775a149d3cd73fc814c17c8c9a606
Guillaume Maudoux <guillaume.maudoux@tweag.io>
2021-10-31 09:28:16 +0100

    bump :-)

--- home/home.nix
+++ home/home.nix
@@ -43,7 +43,7 @@
   nixpkgs.overlays = [
     (self: super: {
       sway-config = super.callPackage ./sway.nix {};
-      lockimage = "/missing"; #TODO
+      lockimage = ./dotfiles/wall-hidpi.png;
     })
     (import ./packages.nix)
   ];
