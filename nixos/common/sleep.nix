11b4577c839fdfdf4743b1b2d3575782d8871042
Guillaume Maudoux <guillaume.maudoux@tweag.io>
2026-06-19 14:10:15 +0200

    Enable slepp target for all normal users

--- nixos/common/sleep.nix
+++ nixos/common/sleep.nix
@@ -1,22 +1,34 @@
 { config, pkgs, lib, ... }:
+let normalUsers = lib.filterAttrs (name: user: user.isNormalUser) config.users.users;
+in
 {
-  systemd.services."user-sleep@" = {
-    description = "Stop user-level unit for user %i";
+  # For each users.users that are isNormalUser, enable
+  systemd.services = (
+    lib.mapAttrs'
+      (username: _: lib.nameValuePair
+        "user-sleep@${username}"
+        { enable = true; }
+      )
+      normalUsers
+  ) // {
+    "user-sleep@" = {
+      description = "Stop user-level unit for user %i";
 
-    # Ensures the service runs only when a string parameter is provided
-    unitConfig = {
-      ConditionNull = false;
-      Before = [ "sleep.target" ];
-      StopWhenUnneeded = true;
-    };
+      # Ensures the service runs only when a string parameter is provided
+      unitConfig = {
+        ConditionNull = false;
+        Before = [ "sleep.target" ];
+        StopWhenUnneeded = true;
+      };
 
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = false;
-      # Uses systemctl --machine to target the specific user's systemd manager (%i)
-      ExecStart = "${pkgs.systemd}/bin/systemctl --machine=%i@ --user start --wait sleep.target";
-      ExecStop = "${pkgs.systemd}/bin/systemctl --machine=%i@ --user stop --wait sleep.target";
-      RequiredBy = [ "sleep.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = false;
+        # Uses systemctl --machine to target the specific user's systemd manager (%i)
+        ExecStart = "${pkgs.systemd}/bin/systemctl --machine=%i@ --user start --wait sleep.target";
+        ExecStop = "${pkgs.systemd}/bin/systemctl --machine=%i@ --user stop --wait sleep.target";
+        RequiredBy = [ "sleep.target" ];
+      };
     };
   };
 
